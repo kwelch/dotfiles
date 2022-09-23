bf239b7b0301261d077cbc698fb2676399756053
Kyle Welch <kwelch0626@gmail.com>
2022-09-23 18:45:16 -0500

    remove  from path since I don't know what it does

--- terminal-helpers/path-alteration.sh
+++ terminal-helpers/path-alteration.sh
@@ -1,7 +1,4 @@
 #!/usr/sh
 
-# Honestly, I don't know what this does
-PATH=$HOME/bin:/usr/local/bin:$PATH
-
 # Add node modules to allow calling local node modules without prefix of yarn|npm
 PATH=$PATH:./node_modules/.bin
